e61b69449b8175d444f13869b98dc804572cb2b8
michelalejo <michelmolina65@gmail.com>
2020-11-22 21:46:13 -0500

    6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 """Type-Annotated Function"""
-from typing import Callable
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: Callable[[int, float], float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Type-Annotated Function"""
     return sum(mxd_lst)

77a9930e70e7619a8a0bb64bea99455ec3f1d8cb
michelalejo <michelmolina65@gmail.com>
2020-11-22 20:05:52 -0500

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Type-Annotated Function"""
+from typing import List
+
+
+def sum_list(imput_list: List[float]) -> float:
+    """Type-Annotated Function"""
+    return sum(imput_list)

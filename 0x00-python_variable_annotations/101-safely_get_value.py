e420b24954be76b132b4db48e4980cab84d488e7
michelalejo <michelmolina65@gmail.com>
2020-11-23 16:47:50 -0500

    101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Type-Annotated Function"""
+from typing import Union, Any, TypeVar, Mapping
+
+
+T = TypeVar("T")
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """Type-Annotated Function"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default

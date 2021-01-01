595acc21d96f3915913137523e3c68290a28a552
michelalejo <michelmolina65@gmail.com>
2020-12-31 19:59:17 -0500

    0-simple_helper_function.py

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+"""The function should return a tuple of size two containing a start index and
+an end index corresponding to the range of indexes to return in a list for
+those particular pagination parameters."""
+
 
 def index_range(page: int, page_size: int):
+    """The function should return a tuple of size two containing a start index and
+an end index corresponding to the range of indexes to return in a list for
+those particular pagination parameters."""
     size = page * page_size
     return (size - page_size, size)

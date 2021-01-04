76e450b45b22ecd526e8f411770cbe49c46aa737
michelalejo <michelmolina65@gmail.com>
2021-01-04 01:23:12 -0500

    filtered_logger.py

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,14 +2,14 @@
 """Function that returns the log message obfuscated."""
 
 from typing import List
-from re import sub
+import re
 
 
 def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
     """Function that returns the log message obfuscated."""
     for pitch in fields:
-        message = sub(pitch + "=.*?" + separator,
+        message = re.sub(pitch + "=.*?" + separator,
                          pitch + "=" + redaction + separator,
                          message)
     return message

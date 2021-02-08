d8a7a1f36d6c0375ebd22b691babc57a4d3d12bd
michelalejo <michelmolina65@gmail.com>
2021-02-08 18:36:46 -0500

    filtered_logger.py

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 """Function that returns the log message obfuscated."""
 
-from typing import List
 import re
+from typing import List
 import logging
 import mysql.connector
 from os import getenv
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
 
 def filter_datum(fields: List[str],
-                 redaction: str, message: str, separator: str) -> str:
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """Function that returns the log message obfuscated."""
-    for pitch in fields:
-        message = re.sub(pitch + "=.*?" + separator,
-                         pitch + "=" + redaction + separator,
-                         message)
+    for item in fields:
+        message = re.sub(fr'{item}=.+?{separator}',
+                         f'{item}={redaction}{separator}', message)
     return message
 
 
@@ -51,10 +54,10 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Redacting Formatter class."""
     db = mysql.connector.connection.MySQLConnection(
-        user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
-        host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=getenv('PERSONAL_DATA_DB_NAME'))
+            user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+            password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+            host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+            database=getenv('PERSONAL_DATA_DB_NAME'))
 
     return db
 

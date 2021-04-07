5aa82748ddb8113fcd347bf6806a1bc3611aca2f
michelalejo <michelmolina65@gmail.com>
2021-04-07 18:26:37 -0500

    session_auth.py

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
+""" API authentication
 """
 
 from api.v1.auth.auth import Auth
+from flask import request
 from typing import List, TypeVar
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):

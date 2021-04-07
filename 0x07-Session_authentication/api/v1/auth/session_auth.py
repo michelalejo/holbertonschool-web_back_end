77ee52bc3a726a605beb24cb132b5569cd7fd046
michelalejo <michelmolina65@gmail.com>
2021-04-07 18:44:45 -0500

    sessin_auth.py

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -28,7 +28,7 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:  
         """returns a User ID based on a Session ID"""
-        if if not session_id or not isinstance(session_id, str):
+        if not session_id or not isinstance(session_id, str):
             return None
 
         return self.user_id_by_session_id.get(session_id)

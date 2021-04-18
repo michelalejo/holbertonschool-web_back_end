6d91c754f071761bd55fbe2726577fcd0a296fd8
michelalejo <michelmolina65@gmail.com>
2021-04-18 03:57:29 -0500

    auth.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -80,5 +80,6 @@ class Auth:
         try:
             user = self._db.find_user_by(id=user_id)
             self._db.update_user(user.id, session_id=None)
+            return None
         except NoResultFound:
             return None

22196af248d7609a7e6e9fa0ab08bd2fdb78d154
michelalejo <michelmolina65@gmail.com>
2021-04-18 03:55:15 -0500

    auth.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -74,12 +74,12 @@ class Auth:
             return None
 
 
-def destroy_session(self, user_id: int) -> None:
-    """Method updates the corresponding user’s session ID to None."""
-    if user_id is None:
-        return None
-    try:
-        user = self._db.find_user_by(id=user_id)
-        self._db.update_user(user.id, session_id=None)
-    except NoResultFound:
-        return None
+    def destroy_session(self, user_id: int) -> None:
+        """Method updates the corresponding user’s session ID to None."""
+        if user_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+        except NoResultFound:
+            return None

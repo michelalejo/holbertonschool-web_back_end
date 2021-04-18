ad03c0468406df9434a43eee5b99c6a507cd81e2
michelalejo <michelmolina65@gmail.com>
2021-04-17 22:33:46 -0500

    auth.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """
 Hash password
 """
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -33,3 +33,12 @@ class Auth:
             return user
 
         raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """It should expect email and password required
+        arguments and return a boolean."""
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False

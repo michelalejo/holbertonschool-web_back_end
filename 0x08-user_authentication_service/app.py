0297a09dfc8f194b5a0ba7b3058cf587c69a77c6
michelalejo <michelmolina65@gmail.com>
2021-04-17 21:59:04 -0500

    app.py

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'], strict_slashes=False)
 def Bienvenue() -> str:
     """Bienvenue."""
-    return jsonify({"message": "Bienvenue"})
+    return jsonify({"message": "Bienvenue"}) 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

4db91cc8ac9dce2de14420141160ad0753b5403a
michelalejo <michelmolina65@gmail.com>
2021-04-18 03:52:32 -0500

    auth.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -79,7 +79,8 @@ def destroy_session(self, user_id: int) -> None:
     if not user_id:
         return None
     try:
-        self._db.update_user(user_id, session_id=None)
+        user = self._db.find_user_by(id=user_id)
+        self._db.update_user(user.id, session_id=None)
         return None
     except NoResultFound:
         return None

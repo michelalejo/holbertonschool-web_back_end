eb19e6c7c9f98cfc5debc8ab7a09d2e751b04284
michelalejo <michelmolina65@gmail.com>
2021-04-17 21:07:15 -0500

    db.py

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,52 +1,55 @@
 #!/usr/bin/env python3
-""" Database for ORM """
-
+"""DB module
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-from typing import TypeVar
+from sqlalchemy.orm.session import Session
+
 from user import Base
 from user import User
 
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 
 class DB:
-    """ DB Class for Object Reational Mappin """
+    """DB class
+    """
 
-    def __init__(self):
-        """ Constructor Method """
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self):
-        """create session """
+    def _session(self) -> Session:
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add a user instance to the session DB """
+        """ Method, which has two required string arguments:
+        email and hashed_password, and returns a User object."""
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        session = self._session
+        session.add(user)
+        session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """ returns the first row found in the users table
-            as filtered by the method’s input arguments
-        """
-        if not kwargs:
+        """ Method takes in arbitrary keyword arguments and returns
+        the first row found in the users table as filtered by
+        the method’s input arguments."""
+        data = User.__table__.columns.keys()
+        if not all(key in data for key in kwargs) or not kwargs:
             raise InvalidRequestError
-
-        user_data = User.__table__.columns.keys()
-        for i in kwargs.keys():
-            if i not in user_data:
-                raise InvalidRequestError
 
         user = self._session.query(User).filter_by(**kwargs).first()
 
@@ -56,17 +59,14 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """ update the user’s attributes as passed in the method’s arguments
-            then commit changes to the database
-        """
+        """ Method that takes as argument a required user_id integer
+        and arbitrary keyword arguments."""
+        session = self._session
         user = self.find_user_by(id=user_id)
+        data = User.__table__.columns.keys()
+        if not all(key in data for key in kwargs) or not kwargs:
+            raise ValueError
 
-        user_data = User.__table__.columns.keys()
-        for i in kwargs.keys():
-            if i not in user_data:
-                raise ValueError
-
-        for i, j in kwargs.items():
-            setattr(user, i, j)
-
-        self._session.commit()
+        for k, v in kwargs.items():
+            setattr(user, k, v)
+        session.commit()

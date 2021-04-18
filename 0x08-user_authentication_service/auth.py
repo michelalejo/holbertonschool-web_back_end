ca85b793a0c662d4dc132210cffd00fee7dcaec5
michelalejo <michelmolina65@gmail.com>
2021-04-18 04:09:20 -0500

    auth.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -83,3 +83,12 @@ class Auth:
             return None
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generate a UUID and update the user."""
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, reset_token=_generate_uuid())
+            return user.reset_token
+        except NoResultFound:
+            raise ValueError

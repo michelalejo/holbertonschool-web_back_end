2d864e2685ed0663831ad3f5e5723057039d8e1b
michelalejo <michelmolina65@gmail.com>
2021-04-18 03:36:56 -0500

    app.py

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -27,5 +27,20 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Create a new session for the user
+    store it the session ID as a cookie"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session = AUTH.create_session(email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', session)
+    return response
+    
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

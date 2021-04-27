318f494fab5a2cda43df5d4dd4c7576701790bf2
michelalejo <michelmolina65@gmail.com>
2021-04-26 22:10:50 -0500

    0-app.py

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Flask App"""
+from flask import Flask, render_template
+from os import getenvs
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Render index.html."""
+    return return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)

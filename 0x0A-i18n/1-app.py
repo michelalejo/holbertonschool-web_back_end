09364f7aa73d3f4d459675e06fcd1b224756be48
michelalejo <michelmolina65@gmail.com>
2021-04-27 02:20:58 -0500

    1-app.py

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Flask App.
+"""
+from flask import Flask, render_template
+from os import getenv
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Config Class."""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """Render index.html."""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)

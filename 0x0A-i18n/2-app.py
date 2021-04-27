e6828166269eb12808c648b461e6d39c1fd6d011
michelalejo <michelmolina65@gmail.com>
2021-04-27 15:26:14 -0500

    2-app.py

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -24,7 +24,7 @@ app.config.from_object(Config)
 @app.route('/')
 def index():
     """Render index.html."""
-    return render_template("0-index.html")
+    return render_template("2-index.html")
 
 
 @babel.localeselector

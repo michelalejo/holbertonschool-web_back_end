df8b9ffdd6737209c13e3e6349be9f2ea190369c
michelalejo <michelmolina65@gmail.com>
2021-04-27 15:26:09 -0500

    1-app.py

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -24,7 +24,7 @@ app.config.from_object(Config)
 @app.route('/')
 def index():
     """Render index.html."""
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":

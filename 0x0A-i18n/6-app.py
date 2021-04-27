d0396b84343793a36a47e28f4c5d28cb997c1d1f
michelalejo <michelmolina65@gmail.com>
2021-04-27 18:11:33 -0500

    6-app.py

---
+++ 0x0A-i18n/6-app.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+Flask App.
+"""
+from flask import Flask, render_template, request
+from flask import g
+from os import getenv
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Config Class."""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """Render index.html."""
+    return render_template("6-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the bestmatch languages."""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    user_id = request.args.get('login_as')
+    if user_id:
+        locale = users[int(user_id)]['locale']
+        if locale in app.config['LANGUAGES']:
+            return locale
+
+    locale = request.headers.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """Returns user dictionary or None"""
+    try:
+        user_id = request.args.get('login_as')
+        return users[int(user_id)]
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """Find a user if any, and set it as
+    a global on flask.g.user. """
+    g.user = get_user()
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)

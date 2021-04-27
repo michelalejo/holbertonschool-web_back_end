f4ccadf634a6d1c3c3457a95b8e651fb4c7d70a2
michelalejo <michelmolina65@gmail.com>
2021-04-27 15:13:12 -0500

    4-app.py

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -31,7 +31,7 @@ def index():
 def get_locale():
     """Determine the bestmatch languages."""
     locale = request.args.get('locale')
-     if locale in app.config['LANGUAGES']:
+    if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 

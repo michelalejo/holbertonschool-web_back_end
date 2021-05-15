003d52c2488e5d992213bf1086ab5887532cd264
michelalejo <michelmolina65@gmail.com>
2021-05-15 15:10:42 -0500

    exercise.py

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,7 @@
 redis.
 """
 from functools import wraps
-from redis import Redis
+import redis
 from typing import Union, Callable, Optional, Any
 import uuid
 
@@ -13,22 +13,11 @@ class Cache:
 
     def __init__(self):
         """__Init__."""
-        self._redis = Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Takes a data argument and returns a string."""
-        uuid = str(uuid.uuid4())
-        self._redis.set(uuid, data)
-        return data
-
-    def get(self, data: str, fn: Optional[Callable] = None) ->\
-            Union[str, bytes, int, float]:
-        """take a key string argument and
-        an optional Callable argument named fn."""
-        if data:
-            res = self._redis.get(data)
-            if fn:
-                return fn(res)
-            else:
-                return res
+        uid = str(uuid.uuid4())
+        self._redis.set(uid, data)
+        return uid

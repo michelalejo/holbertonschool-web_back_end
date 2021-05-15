4da586e5d233bd6a986baaa312fa5fe3f5c3b2eb
michelalejo <michelmolina65@gmail.com>
2021-05-15 15:12:48 -0500

    exercise.py

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -21,3 +21,24 @@ class Cache:
         uid = str(uuid.uuid4())
         self._redis.set(uid, data)
         return uid
+
+    def get(self, data: str, fn: Optional[Callable] = None) ->\
+            Union[str, bytes, int, float]:
+        """take a key string argument and
+        an optional Callable argument named fn."""
+        if data:
+            res = self._redis.get(data)
+            if fn:
+                return fn(res)
+            else:
+                return res
+    
+    def get_str(self, data: bytes) -> str:
+        """Automatically parametrize Cache.get with
+        the correct conversion function."""
+        return data.decode("utf-8")
+
+    def get_int(self, data: bytes) -> int:
+        """Automatically parametrize Cache.get with
+        the correct conversion function."""
+        return int(data)

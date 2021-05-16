bf292ccc6a5eab7e745cd16b11be8cb8dc8bda31
michelalejo <michelmolina65@gmail.com>
2021-05-16 01:32:48 -0500

    web.py

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Track how many times a particular URL was accessed."""
+from redis.client import Redis
+import requests
+
+
+redis = Redis()
+i = 0
+
+
+def get_page(url: str) -> str:
+    """Track how many times a particular URL was accessed."""
+    data = f"count:{url}"
+    redis.set(data, i)
+    res = requests.get(url)
+    redis.incr(data)
+    redis.setex(data, 10, redis.get(data))
+    return res.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')

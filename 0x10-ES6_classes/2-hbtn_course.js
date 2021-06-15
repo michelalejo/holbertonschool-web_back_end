c9f815872b866b310d6da498ba894851177e2889
michelalejo <michelmolina65@gmail.com>
2021-06-14 20:02:57 -0500

    2-hbtn_course.js

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    if (Array.isArray(students) !== true) throw TypeError('Students must be an array');
+    for (const student of students) if (typeof student !== 'string') throw TypeError('Students must be strings');
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    if (Array.isArray(students) !== true) throw TypeError('Students must be an array');
+    for (const student of students) if (typeof student !== 'string') throw TypeError('Students must be strings');
+    this._students = students;
+  }
+
+  get students() {
+    return this._students;
+  }
+}

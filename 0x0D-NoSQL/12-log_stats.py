131d8fb9fcb79a61db47a39d868bec2a72b8e178
michelalejo <michelmolina65@gmail.com>
2021-06-14 19:09:57 -0500

    12-log_stats.py

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+    Python function
+"""
+from pymongo import MongoClient
+
+
+def logger(a: dict) -> int:
+    """return logger"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_logs = client.logs.nginx
+    return nginx_logs.count_documents(a)
+
+
+def main():
+    """
+    Nginx logs stored in MongoDB
+    """
+    print(f"{ logger({}) } logs")
+    print("Methods:")
+    print(f"\tmethod GET: { logger({'method': 'GET'}) }")
+    print(f"\tmethod POST: { logger({'method': 'POST'}) }")
+    print(f"\tmethod PUT: {logger({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {logger({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {logger({'method': 'DELETE'})}")
+    print(f"{logger({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    main()

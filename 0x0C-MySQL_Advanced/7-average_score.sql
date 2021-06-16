6a92741bb2b877cc099aeed4eee2e25f32532bbd
michelalejo <michelmolina65@gmail.com>
2021-06-16 09:34:30 -0500

    7-average_score.sql

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,15 @@
+-- Write a SQL script that creates a stored procedure
+-- ComputeAverageScoreForUser that computes and store
+-- the average score for a student. Note: An average score can be a decimal
+delimiter //
+CREATE PROCEDURE ComputeAverageScoreForUser (
+	IN user_id INT
+)
+BEGIN
+	UPDATE users
+	SET average_score = (SELECT SUM(score) / COUNT(*)
+   						 FROM corrections AS c
+   						 WHERE c.user_id = user_id)
+	WHERE id = user_id;
+END;
+//
